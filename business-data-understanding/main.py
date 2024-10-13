ab3002814ad144201a6ec20d2b493ff0511ab2e5
rulanugrh <nextarituenak01@gmail.com>
2024-10-13 20:45:27 +0700

    feat: adding sample parameter

--- business-data-understanding/main.py
+++ business-data-understanding/main.py
@@ -4,7 +4,33 @@
 # print(f"Download: {path}")
 
 import pandas as pd
+import sys
 
-data = pd.read_csv('./fashion_products.csv')
-df = pd.DataFrame(data)
-df[df["Rating"] > 4].to_csv("fashion_products_rate.csv")
+df = pd.DataFrame(pd.read_csv('./fashion_products.csv'))
+data = df[df["Rating"] > 4]
+
+def info():
+    return data.info()
+
+def get_null():
+    msg = ""
+
+    if data.isnull().sum().sum() <= 0:
+        msg += "Data tidak ada yang Null"
+    else:
+        msg += f"Total data null adalah: {data.isnull().sum().sum()}"
+    
+    return msg
+
+def main(argv: str):
+    if argv == "info":
+        info()
+    elif argv == "get_null":
+        print(get_null())
+    else:
+        print("Sorry invalid command: \npython main.py [args] \n\ninfo (for get info dataset) \nnull (for get data null) \nhistogram (for get histogram data)\n")
+
+if __name__ == "__main__":
+
+    arg = sys.argv[1]
+    main(argv=arg)
