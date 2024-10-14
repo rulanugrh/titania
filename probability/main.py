fad1bc9123e997d96fca0bf8f7ef27f55a38d3aa
rulanugrh <nextarituenak01@gmail.com>
2024-10-14 14:54:39 +0700

    chore: adding probability and loop probability function

--- probability/main.py
+++ probability/main.py
@@ -1,12 +1,51 @@
-import math
 from typing import List, Optional
 
+def factorial(num: int) -> int:
+    fct = 1
+    if num < 0:
+        return 0
+    elif num == 0:
+        return 1
+    else:
+        for i in range(1, num + 1):
+            fct = fct * i
+        
+        return fct
+
+def probability(m: int,  n: int, opsi: str = "dadu", flag: str = ">") -> int:
+    if opsi == "dadu":
+        if flag == ">":
+            a = 6 - m
+            b = a / 6
+            res = [ b for x in range(n)]
+            return sum(res)
+        elif flag == "<":
+            a = 5 - (6 - m)
+            b = a / 6
+            res = [ b for x in range(n)]
+            return sum(res)
+        else:
+            return sum([(m / 6) for x in range(n)])
+    else:
+        return m / n
+
+def loop_probability(m: int, n: int, count: int):
+    res = m / n
+    for i in range(count):
+        if i > 0:
+            a = m - 1
+            b = a / (n - 1)
+            res *= b
+        
+    
+    return res
+
 def permutation(arr: List[str] | List[int], x: int) -> int | float:
-    res = math.factorial(len(arr)) / math.factorial((len(arr) - x))
+    res = factorial(len(arr)) / factorial((len(arr) - x))
     return res
 
 def combination(n: int, k: int) -> int | float:
-    res = math.factorial(n) / (math.factorial(k) * math.factorial((n - k)))
+    res = factorial(n) / (factorial(k) * factorial((n - k)))
     return res
 
 def harapan(x: List[int], y: List[int], n: int) -> int:
@@ -21,3 +60,8 @@ if __name__ == "__main__":
     print(f"Banyak anggota yang terbentuk: {combination(10, 3)}")
     print(f"Keanggotaan terdiri dari 2 Pria dan 1 Wanita: {combination(4, 2) * combination(6, 1)}")
     print(f"Banyak cara Minimal 2 Perempuan: {harapan(x=[2, 3, 4], y=[3, 2, 1], n=8)}")
+    print(f"Banyak peluang dadu yang lebih dari 4 dalam 6 iterasi adalah: {probability(4, 6, "dadu", ">")}")
+
+    print(f"Probabilitas bola kedua bewarna biru adalah: {loop_probability(m=4, n=7, count=0)}")
+    print(f"Harapan dari X: {harapan(x=[1, 2, 3, 4, 5, 6], y=[1, 1, 1, 1, 1, 1], n=6)}")
+    print(f"Kedua bola bewarna putih: {loop_probability(m=7, n=10, count=2)}")
