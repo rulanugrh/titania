25aae50c6bcc30e332ae0b679d8ee07f03ce9928
rulanugrh <nextarituenak01@gmail.com>
2024-10-14 09:27:37 +0700

    chore: adding function for harapan matematik

--- probability/main.py
+++ probability/main.py
@@ -9,9 +9,15 @@ def combination(n: int, k: int) -> int | float:
     res = math.factorial(n) / (math.factorial(k) * math.factorial((n - k)))
     return res
 
+def harapan(x: List[int], y: List[int], n: int) -> int:
+    k = [i / n for i in y]
+    multiply = [a * b for a, b in zip(x, k)]
+    return sum(multiply)
+
 if __name__ == "__main__":
     arr = ["A", "B", "C", "D", "E", "F"]
 
     print(f"Banyak susunan dari 3 huruf: {permutation(arr, 3)}")
     print(f"Banyak anggota yang terbentuk: {combination(10, 3)}")
     print(f"Keanggotaan terdiri dari 2 Pria dan 1 Wanita: {combination(4, 2) * combination(6, 1)}")
+    print(f"Banyak cara Minimal 2 Perempuan: {harapan(x=[2, 3, 4], y=[3, 2, 1], n=8)}")
